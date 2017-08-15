240784bd7fa4ff2cdbac472658f66835e67b4805
Erik Larkö <erik@larko.se>
2017-08-15 09:39:26 +0200

    Very basic probably non-functioning router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,53 @@ import React, { Component } from 'react';
 import { CalcGraph } from './my-stuff/components/CalcGraph';
 
 import { Recipe, LeSec } from './my-stuff/le-sec-test/recipe';
+import { Main } from './my-stuff/main.js';
 
 class App extends Component {
 
     render() {
         const leSec = new Recipe(LeSec);
-        return <div id='apa'>
-            <CalcGraph recipe={ leSec } />
-        </div>
+        const routes = new Map();
+        routes.set('/', <Main a={1}/>);
+        routes.set('/graph', <CalcGraph recipe={ leSec } />);
+
+        return <Router routes={ routes } />
+    }
+}
+
+class Router extends Component {
+
+    state: {
+        path: string,
+    };
+    props: {
+        routes: Map<string, React$Element<any>>,
+        defaultRoute?: React$Element<any>,
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            path: window.location.pathname,
+        };
+    }
+
+    componentDidMount() {
+        window.onpopstate = (e) => {
+            console.log('ON POP STATE', e);
+        };
+    }
+
+    componentWillUnmount() {
+        window.onpopstate = undefined;
+    }
+
+
+    render() {
+        console.log('rendering', this.state.path);
+        const component = this.props.routes.get(this.state.path) || this.props.defaultRoute || <div>No route :( {this.state.path} </div>
+
+        return component;
     }
 }
 
